e3eaeb72589fd7712d50263ddb46aad34188cecb
705912 <705912@D-SOUT-166-29.athena.root>
2022-05-03 12:46:13 -0700

    Thingy

---
+++ WebEngine/src/security/Permisions.java
@@ -0,0 +1,53 @@
+package security;
+
+import java.util.HashMap;
+
+import dataManagment.JsonObj;
+import dataManagment.JsonSerializable;
+
+public class Permisions implements JsonSerializable {
+	
+	private HashMap<String, Perm> perms;
+	
+	public Permisions() {
+		perms = new HashMap<String, Perm>();
+	}
+	public Permisions(JsonObj obj) {
+		perms = new HashMap<String, Perm>();
+		deserialize(obj);
+	}
+	
+	public void setPerm(String perm, boolean has) {
+		setPerm(perm.split("."), has);
+	}
+	public void setPerm(String[] perm, boolean has) {
+		if(perms.containsKey(perm[0])) {
+			perms.get(perm[0]).setPerm(perm, has);
+		} else {
+			perms.put(perm[0], new Perm(perm, 0));
+		}
+	}
+	
+	public boolean hasPerm(String perm) {
+		return hasPerm(perm.split("."));
+	}
+	public boolean hasPerm(String[] perm) {
+		if(perms.containsKey(perm[0])) {
+			return perms.get(perm[0]).hasPerm(perm);
+		}
+		return false;
+	}
+
+	@Override
+	public JsonObj serialize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deserialize(JsonObj obj) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
